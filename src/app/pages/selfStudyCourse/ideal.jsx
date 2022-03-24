e2369d0b8f5c0489f90ddc7ffce4c2b52758104a
Zarah <60463624+FatimeyiZahra@users.noreply.github.com>
2022-03-24 06:40:16 -0700

    video player

--- src/app/pages/selfStudyCourse/ideal.jsx
+++ src/app/pages/selfStudyCourse/ideal.jsx
@@ -14,6 +14,34 @@ function callback(key) {
 }
 const { Panel } = Collapse;
 
+const onVideoEnd = () => {
+  console.log("Video ended");
+};
+const onVideoTimeUpdate = (duration) => {
+  console.log("Time updated: ", duration);
+};
+
+const onPlayerReady = (player) => {
+  console.log("Player is ready: ", player);
+  // this.player = player;
+};
+
+const onVideoPlay = (duration) => {
+  console.log("Video played at: ", duration);
+};
+
+const onVideoPause = (duration) => {
+  console.log("Video paused at: ", duration);
+};
+
+const onVideoSeeking = (duration) => {
+  console.log("Video seeking: ", duration);
+};
+
+const onVideoSeeked = (from, to) => {
+  console.log(`Video seeked from ${from} to ${to}`);
+};
+
 const SelfStudyCourse = () => {
   // player = {};
   const [state, setfirst] = useState({
@@ -22,37 +50,6 @@ const SelfStudyCourse = () => {
       poster: "https://video-react.js.org/assets/poster.png",
     },
   });
-  const [duration, setduration] = useState();
-  console.log(duration);
-
-  const onVideoEnd = () => {
-    console.log("Video ended");
-  };
-  const onVideoTimeUpdate = (duration) => {
-    console.log("Time updated: ", duration);
-  };
-
-  const onPlayerReady = (player) => {
-    console.log("Player is ready: ", player);
-    setduration(player.cache_);
-    // this.player = player;
-  };
-
-  const onVideoPlay = (duration) => {
-    console.log("Video played at: ", duration);
-  };
-
-  const onVideoPause = (duration) => {
-    console.log("Video paused at: ", duration);
-  };
-
-  const onVideoSeeking = (duration) => {
-    console.log("Video seeking: ", duration);
-  };
-
-  const onVideoSeeked = (from, to) => {
-    console.log(`Video seeked from ${from} to ${to}`);
-  };
 
   return (
     <div style={{ marginLeft: "15px" }}>
@@ -60,11 +57,10 @@ const SelfStudyCourse = () => {
         <Col sm={24} xs={24} md={16} lg={16}>
           <div>
             <VideoPlayer
-              className="videoResponsive"
               controls={true}
               src={state.video.src}
               poster={state.video.poster}
-              width="720"
+              // width="720"
               height="420"
               onReady={onPlayerReady}
               onPlay={onVideoPlay}
