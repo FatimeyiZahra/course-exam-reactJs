75b22117e0c1bbc641df3b3485ac1da4f71737ff
FatimeyiZahra <fatimeyizahrai@gmail.com>
2022-07-20 20:08:19 +0400

    css

--- src/app/pages/documentation/Usage.jsx
+++ src/app/pages/documentation/Usage.jsx
@@ -12,16 +12,25 @@ const Usage = () => {
           according to your company regulations) to answer. Please note that you
           should answer all questions.
         </li>
-        <li>After answering all questions please submit with "Submit" button.</li>
-        <li className="my-4">After clicking the submit button, you can see your exam result.</li>
+        <li>
+          After answering all questions please submit with "Submit" button.
+        </li>
+        <li className="my-4">
+          After clicking the submit button, you can see your exam result.
+        </li>
         <li>You can select any question by number to review.</li>
       </ul>
       <header className="my-4">
         <h1 className="doc_title">Video Course Usage</h1>
       </header>
-      <ul>
-        <li>You have to watch the video from start to end without forward. Othrwise the video'll be marked as unwatched.</li>
-      </ul>
+      <div className="alert alert-danger fade show" role="alert">
+        <h4 className="alert-heading">Important</h4>
+        <div className="alert-body">
+          You have to watch the video from start to end without forward.
+          Othrwise the video'll be marked as unwatched
+        </div>
+      </div>
+      <ul></ul>
     </article>
   );
 };
